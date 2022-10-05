6457ceb3dca93159941acd752ef16f55b160a3e9
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-10-05 11:54:18 -0300

    integracao-mongo-final

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -15,10 +15,6 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comments')
 export class CommentController {
-  // private commentService = new CommentService(HttpService as any);
-
-  // Quando criar o construtor da classe, passa por debaixo dos panos o parametro commentService do tipo CommentService
-  // parametro commentService é innjetado nessa classe
   constructor(private readonly commentService: CommentService) {}
 
   @Post()
@@ -33,18 +29,18 @@ export class CommentController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
+    return this.commentService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
+    return this.commentService.update(id, updateCommentDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+    return this.commentService.remove(id);
   }
 
   @Get('user/:id')
--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommentSchema } from './comment.schema';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([
+      {
+        name: 'Comment',
+        schema: CommentSchema,
+      },
+    ]),
+  ],
   controllers: [CommentController],
   providers: [CommentService],
 })
